25c1e07017b45b55bfcf8dffc297106015af6bd3
AVC0706 <atharvvijay4@gmail.com>
2021-02-19 01:10:14 +0530

    FCM integrated

--- src/App.js
+++ src/App.js
@@ -1,56 +1,49 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./Pages/Home";
+import React, {useEffect, useState} from "react";
+import {BrowserRouter as Router} from "react-router-dom";
 import Navbar from "./Navbar";
-import Glimpses from "./Pages/previous-glimpses/Glimpses";
-import Auth from "./Pages/Login_SignUp/auth/auth";
-import About from "./Pages/About_Page";
-import Team from "./Pages/Team";
-import Events from "./Pages/Events";
-import Sponsor from "./Pages/Sponsors/Sponsor";
-import Particles from "react-particles-js";
 import Footer from "./Footer";
 import Preloader from "./Pages/Preloader/Preloader";
-import Suspense from "react";
 import Routes from "./routes";
 import AuthContextProvider from "./context/Auth/AuthContext";
 import SnackbarContextProvider from "./context/Snackbar/SnackbarContext";
 import MySnackbar from "./EMS/components/Snackbar";
+import Notification from "./components/notification";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    console.log("Loading");
-  }, [loading]);
+    useEffect(() => {
+        console.log("Loading");
+    }, [loading]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    }, []);
 
-  return (
-    <div className="App">
-      <SnackbarContextProvider>
-        <AuthContextProvider>
-          <Router>
-            {loading ? (
-              <Preloader />
-            ) : (
-              <>
-                <Navbar />
-                <Routes />
-                <Footer />
-              </>
-            )}
-          </Router>
-          <MySnackbar />
-        </AuthContextProvider>
-      </SnackbarContextProvider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <SnackbarContextProvider>
+                <AuthContextProvider>
+                    <Router>
+                        {loading ? (
+                            <Preloader/>
+                        ) : (
+                            <>
+                                <Navbar/>
+                                <Notification/>
+                                <Routes/>
+                                <Footer/>
+                            </>
+                        )}
+                    </Router>
+                    <MySnackbar/>
+                </AuthContextProvider>
+            </SnackbarContextProvider>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,41 @@
+import firebase from 'firebase/app';
+import 'firebase/messaging';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDo5Cur-WMaSuwaMpYKj2ApFhiJ9k8BirY",
+    authDomain: "pulzion-21.firebaseapp.com",
+    projectId: "pulzion-21",
+    storageBucket: "pulzion-21.appspot.com",
+    messagingSenderId: "291337681434",
+    appId: "1:291337681434:web:1e0a5d645306ecce046fa3",
+    measurementId: "G-QYYSV67LY3"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+export const getToken = (setTokenFound) => {
+    return messaging.getToken({vapidKey: process.env["REACT_APP_FCM_KEY"]}).then((currentToken) => {
+        if (currentToken) {
+            console.log('current token for client: ', currentToken);
+            setTokenFound(true);
+            // Track the token -> client mapping, by sending to backend server
+            // show on the UI that permission is secured
+        } else {
+            console.log('No registration token available. Request permission to generate one.');
+            setTokenFound(false);
+            // shows on the UI that permission is required
+        }
+    }).catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+        // catch error while creating client token
+    });
+}
+
+export const onMessageListener = () =>
+    new Promise((resolve) => {
+        messaging.onMessage((payload) => {
+            resolve(payload);
+        });
+    });
