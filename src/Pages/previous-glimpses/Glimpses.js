3800e424b31f84308c8801534320c3938be1fd6c
Pratik Daigavane <daigavanep@gmail.com>
2021-02-22 14:18:37 +0530

    nanogallery added

--- src/Pages/previous-glimpses/Glimpses.js
+++ src/Pages/previous-glimpses/Glimpses.js
@@ -1,16 +1,12 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {
     Grid,
     Typography,
     createMuiTheme,
     responsiveFontSizes,
-    MuiThemeProvider,
     makeStyles,
 } from "@material-ui/core";
-import Header from "./header";
-import Content from "./Content";
 import "./Glimpses.css";
-
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
 
@@ -21,6 +17,94 @@ const useStyles = makeStyles({
 });
 
 function Glimpses() {
+    useEffect(()=>{
+        window.jQuery("#nanogallery").nanogallery2({
+
+            // CONTENT SOURCE
+            // The optional add-on nanoPhotosProvider is used for this example - this is not mandatory and can easily be replaced by a list of medias
+            kind: 'nano_photos_provider2',
+            dataProvider: 'https://pict.acm.org/gallery/nano_photos_provider2.php',
+
+            // GALLERY AND THUMBNAIL LAYOUT
+            galleryMosaic : [                       // default layout
+                { w: 2, h: 2, c: 1, r: 1 },
+                { w: 1, h: 1, c: 3, r: 1 },
+                { w: 1, h: 1, c: 3, r: 2 },
+                { w: 1, h: 2, c: 4, r: 1 },
+                { w: 2, h: 1, c: 5, r: 1 },
+                { w: 2, h: 2, c: 5, r: 2 },
+                { w: 1, h: 1, c: 4, r: 3 },
+                { w: 2, h: 1, c: 2, r: 3 },
+                { w: 1, h: 2, c: 1, r: 3 },
+                { w: 1, h: 1, c: 2, r: 4 },
+                { w: 2, h: 1, c: 3, r: 4 },
+                { w: 1, h: 1, c: 5, r: 4 },
+                { w: 1, h: 1, c: 6, r: 4 }
+            ],
+            galleryMosaicXS : [                     // layout for XS width
+                { w: 2, h: 2, c: 1, r: 1 },
+                { w: 1, h: 1, c: 3, r: 1 },
+                { w: 1, h: 1, c: 3, r: 2 },
+                { w: 1, h: 2, c: 1, r: 3 },
+                { w: 2, h: 1, c: 2, r: 3 },
+                { w: 1, h: 1, c: 2, r: 4 },
+                { w: 1, h: 1, c: 3, r: 4 }
+            ],
+            galleryMosaicSM : [                     // layout for SM width
+                { w: 2, h: 2, c: 1, r: 1 },
+                { w: 1, h: 1, c: 3, r: 1 },
+                { w: 1, h: 1, c: 3, r: 2 },
+                { w: 1, h: 2, c: 1, r: 3 },
+                { w: 2, h: 1, c: 2, r: 3 },
+                { w: 1, h: 1, c: 2, r: 4 },
+                { w: 1, h: 1, c: 3, r: 4 }
+            ],
+            galleryMaxRows: 1,
+            galleryDisplayMode: 'rows',
+            gallerySorting: 'random',
+            thumbnailDisplayOrder: 'random',
+
+            thumbnailHeight: '180', thumbnailWidth: '220',
+            thumbnailAlignment: 'scaled',
+            thumbnailGutterWidth: 0, thumbnailGutterHeight: 0,
+            thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+
+            thumbnailToolbarImage: null,
+            thumbnailToolbarAlbum: null,
+            thumbnailLabel: { display: false },
+
+            // DISPLAY ANIMATION
+            // for gallery
+            galleryDisplayTransitionDuration: 1500,
+            // for thumbnails
+            thumbnailDisplayTransition: 'imageSlideUp',
+            thumbnailDisplayTransitionDuration: 1200,
+            thumbnailDisplayTransitionEasing: 'easeInOutQuint',
+            thumbnailDisplayInterval: 60,
+
+            // THUMBNAIL HOVER ANIMATION
+            thumbnailBuildInit2: 'image_scale_1.15',
+            thumbnailHoverEffect2: 'thumbnail_scale_1.00_1.05_300|image_scale_1.15_1.00',
+            touchAnimation: true,
+            touchAutoOpenDelay: 500,
+
+            // LIGHTBOX
+            viewerToolbar: { display: false },
+            viewerTools:    {
+                topLeft:   'label',
+                topRight:  'shareButton, rotateLeft, rotateRight, fullscreenButton, closeButton'
+            },
+
+            // GALLERY THEME
+            galleryTheme : {
+                thumbnail: { background: '#111' },
+            },
+
+            // DEEP LINKING
+            locationHash: true
+        });
+
+    })
     const classes = useStyles();
     return (
         <Grid container direction="column" style={{zIndex: 10}}>
@@ -43,18 +127,8 @@ function Glimpses() {
                     <Grid item xs={0} sm={2}/>
                 </div>
             </Grid>
+            <div id="nanogallery"></div>
 
-            <Grid item container>
-                <div className="underline"></div>
-            </Grid>
-
-            <Grid item container>
-                <Grid item xs={1} sm={1}/>
-                <Grid item xs={10} sm={10}>
-                    <Content/>
-                </Grid>
-                <Grid item xs={1} sm={1}/>
-            </Grid>
         </Grid>
     );
 }
