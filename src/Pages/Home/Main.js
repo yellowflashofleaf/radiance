519562887501941e37c6b46f7430854dae80d31c
rohitpardeshi04 <rohitmpardeshi0411@gmail.com>
2021-02-23 22:10:09 +0530

    dates

--- src/Pages/Home/Main.js
+++ src/Pages/Home/Main.js
@@ -1,100 +1,113 @@
-import React, {useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import "./Main.css";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import {AuthContext} from "../../context/Auth/AuthContext";
+import { AuthContext } from "../../context/Auth/AuthContext";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 export default function Main() {
-    const history = useHistory();
-    AOS.init();
+  const history = useHistory();
+  AOS.init();
 
-    const authContext = useContext(AuthContext);
-    const {isAuth} = authContext;
-    useEffect(() => {
-        window.VANTA.GLOBE({
-            el: "#your-element-selector",
-            mouseControls: true,
-            touchControls: true,
-            gyroControls: true,
-            minHeight: 300.0,
-            minWidth: 300.0,
-            width: "98vw",
+  const authContext = useContext(AuthContext);
+  const { isAuth } = authContext;
+  useEffect(() => {
+    window.VANTA.GLOBE({
+      el: "#your-element-selector",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: true,
+      minHeight: 300.0,
+      minWidth: 300.0,
+      width: "98vw",
+      height: "90vh",
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: 0xe4ff,
+      color2: 0x92c3c3,
+      backgroundColor: 0x0,
+    });
+  }, []);
+
+  return (
+    <>
+      <div
+        className="main"
+        style={{
+          maxHeight: "90vh",
+          overflow: "hidden !important",
+        }}
+      >
+        <div
+          id="your-element-selector"
+          style={{
+            position: "absolute",
+            margin: "0 -5%",
             height: "90vh",
-            scale: 1.0,
-            scaleMobile: 1.0,
-            color: 0xe4ff,
-            color2: 0x92c3c3,
-            backgroundColor: 0x0,
-        });
-    }, []);
-
-    return (
-        <>
-            <div
-                className="main"
-                style={{
-                    maxHeight: "90vh",
-                    overflow: "hidden !important",
-                }}
-            >
-                <div id="your-element-selector"
-                    style={{
-                        position: "absolute",
-                        margin: "0 -5%",
-                        height: "90vh",
-                        zIndex: "1",
-                    }}
-                ><></></div>
-                <div className="title" style={{zIndex: "10", position: "absolute"}}>
-                    PULZION
-                    <span
-                        style={{
-                            color: "rgba(22, 164, 189, 0.9)",
-                            fontSize: "inherit",
-                        }}
-                    >
+            zIndex: "1",
+          }}
+        >
+          <></>
+        </div>
+        <div className="title" style={{ zIndex: "10", position: "absolute" }}>
+          PULZION
+          <span
+            style={{
+              color: "rgba(22, 164, 189, 0.9)",
+              fontSize: "inherit",
+            }}
+          >
             '
           </span>
-                    21
-                </div>
-                <div
-                    className="subtitle"
-                    data-aos="fade-up"
-                    data-aos-offset="150"
-                    data-aos-easing="ease-in-sine"
-                    duration="900"
-                >
-                    The Annual Technical Fest of PASC
-                </div>
-                <div
-                    className="reg-btn"
-                    data-aos="flip-up"
-                    data-aos-offset="150"
-                    data-aos-easing="ease-in-sine"
-                    duration="900"
-                    style={{position: "absolute", zIndex: "10", marginTop: "20%"}}
-                >
-                    <button
-                        className="shrink-border"
-                        onClick={() => {
-                            isAuth ? history.push("/myevents") : history.push("/login");
-                        }}
-                    >
-                        {" "}
-                        {isAuth ? (
-                            <>
-                                MY EVENTS <ArrowForwardIosIcon/>
-                            </>
-                        ) : (
-                            <>
-                                REGISTER NOW! <ArrowForwardIosIcon/>
-                            </>
-                        )}
-                    </button>
-                </div>
-            </div>
-        </>
-    );
+          21
+        </div>
+        <div
+          className="subtitle"
+          data-aos="fade-up"
+          data-aos-offset="150"
+          data-aos-easing="ease-in-sine"
+          duration="900"
+        >
+          The Annual Technical Fest of PASC
+        </div>
+        <div
+          className="reg-btn"
+          data-aos="flip-up"
+          data-aos-offset="150"
+          data-aos-easing="ease-in-sine"
+          duration="900"
+          style={{ position: "absolute", zIndex: "10", marginTop: "20%" }}
+        >
+          <button
+            className="shrink-border"
+            onClick={() => {
+              isAuth ? history.push("/myevents") : history.push("/login");
+            }}
+          >
+            {" "}
+            {isAuth ? (
+              <>
+                MY EVENTS <ArrowForwardIosIcon />
+              </>
+            ) : (
+              <>
+                REGISTER NOW! <ArrowForwardIosIcon />
+              </>
+            )}
+          </button>
+        </div>
+        <div
+          className="coming-soon"
+          data-aos="fade-up"
+          data-aos-offset="150"
+          data-aos-easing="ease-in-sine"
+          duration="900"
+          //   style={{ marginTop: "35%", fontSize: "1.5rem" }}
+        >
+          5th, 6th, 7th March 2021
+        </div>
+      </div>
+    </>
+  );
 }
