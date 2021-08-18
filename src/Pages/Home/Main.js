693cb6fb4adb735546f85f790c686f491f40c511
SiddhiWakchaure <60885429+SiddhiWakchaure@users.noreply.github.com>
2021-08-18 17:14:01 +0530

    Home page alignment

--- src/Pages/Home/Main.js
+++ src/Pages/Home/Main.js
@@ -38,103 +38,130 @@ export default function Main() {
         className="main"
         style={{
           maxHeight: "90vh",
-          // overflow: "hidden !important",
         }}
       >
         <div
           id="your-element-selector"
           style={{
             position: "absolute",
-            margin: "0 -5%",
+            // margin: "0 -5%",
             height: "90vh",
             zIndex: "1",
           }}
         >
           <></>
         </div>
-        <div className="spon"></div>
-        <div className="title" style={{ zIndex: "10", position: "absolute" }}>
-          <div className="col-md-4 p-3 text-center">
-            {/* <img src="/Logos/GigIndia_horiz.png" /> */}
-          </div>
-          {/* <div
+        <div className="homepage-container">
+          {/* <div className="spon"></div> */}
+          <div className="title text-center">
+            <div>
+              <img
+                src="/Logos/GigIndia_horiz.png"
+                style={{ height: "20%", width: "20%" }}
+                className="title-sponsor"
+              />
+            </div>
+            {/* <div
             style={{ fontSize: "1rem", textAlign: "center" }}
             className="mt-1 col-md-4"
           >
             presents
           </div> */}
-          <span className="hii">
-            Radiance
-            <span
-              style={{
-                color: "#990090",
-                fontSize: "inherit",
-              }}
-            >
-              '
+            <span className="hii">
+              Radiance
+              <span
+                style={{
+                  color: "#990090",
+                  fontSize: "inherit",
+                }}
+              >
+                '
+              </span>
+              21
             </span>
-            21
-          </span>
-        </div>
-        <div
-          className="subtitle"
-          data-aos="fade-up"
-          data-aos-offset="150"
-          data-aos-easing="ease-in-sine"
-          duration="900"
-        >
-          The Annual Technical Fest of PASC-W
-        </div>
-        <br />
-        <br />
+          </div>
 
-        {/* <div
-          className="subtitle row"
-          data-aos="fade-up"
-          data-aos-offset="150"
-          data-aos-easing="ease-in-sine"
-          duration="900"
-        >
-          <Typography style={{ color: "#fff", paddingLeft: 0 }}>
+          <div
+            className="subtitle"
+            data-aos="fade-up"
+            data-aos-offset="150"
+            data-aos-easing="ease-in-sine"
+            duration="900"
+          >
+            The Annual Technical Fest of PASC-W
+          </div>
+          <br />
+          <br />
+          <Typography
+            style={{ color: "#fff", marginTop: -20, fontWeight: "600" }}
+            className="copoweredByText"
+          >
             co-powered by
           </Typography>
-          <div className="col-sm-2 py-3 text-center">
-            <img src="/Logos/Nitor Infotech.png" />
-          </div>
-          <div className="col-sm-2 py-3 text-center">
-            <img src="/Logos/iMocha.png" />
-          </div>
-        </div> */}
-        <div
-          className="reg-btn"
-          style={{ position: "absolute", zIndex: "10", marginTop: "20%" }}
-        >
-          <button
-            className="shrink-border"
-            onClick={() => {
-              isAuth ? history.push("/myevents") : history.push("/login");
-            }}
+          <div
+            className="row d-flex justify-content-center"
+            data-aos="fade-up"
+            data-aos-offset="150"
+            data-aos-easing="ease-in-sine"
+            duration="900"
           >
-            {isAuth ? (
-              <>
-                MY EVENTS <ArrowForwardIosIcon />
-              </>
-            ) : (
-              <>
-                REGISTER NOW! <ArrowForwardIosIcon />
-              </>
-            )}
-          </button>
-        </div>
-        <div
-          className="coming-soon"
-          //   data-aos="fade-up"
-          //   data-aos-offset="150"
-          //   data-aos-easing="ease-in-sine"
-          //   duration="900"
-          //   style={{ marginTop: "35%", fontSize: "1.5rem" }}
-        >
-          3rd, 4th, 5th September 2021
+            <div className="col-sm-2 col-md-3 py-3">
+              <img
+                src="/Logos/Nitor Infotech.png"
+                width="60%"
+                className="copoweredBy"
+              />
+            </div>
+            <div className="col-sm-2 col-md-3 py-3">
+              <img
+                src="/Logos/iMocha.png"
+                width="60%"
+                className="copoweredBy"
+              />
+            </div>
+            <div className="col-sm-2 col-md-3 py-3">
+              <img
+                src="/Logos/iMocha.png"
+                width="60%"
+                className="copoweredBy"
+              />
+            </div>
+            <div className="col-sm-2 col-md-3 py-3">
+              <img
+                src="/Logos/iMocha.png"
+                width="60%"
+                className="copoweredBy"
+              />
+            </div>
+          </div>
+          <div className="reg-btn">
+            <button
+              className="shrink-border"
+              onClick={() => {
+                isAuth ? history.push("/myevents") : history.push("/login");
+              }}
+            >
+              {isAuth ? (
+                <>
+                  MY EVENTS <ArrowForwardIosIcon />
+                </>
+              ) : (
+                <>
+                  REGISTER NOW! <ArrowForwardIosIcon />
+                </>
+              )}
+            </button>
+          </div>
+          <div
+            className="coming-soon"
+            //   data-aos="fade-up"
+            //   data-aos-offset="150"
+            //   data-aos-easing="ease-in-sine"
+            //   duration="900"
+            //   style={{ marginTop: "35%", fontSize: "1.5rem" }}
+          >
+            3rd, 4th, 5th September 2021
+          </div>
         </div>
       </div>
     </>
